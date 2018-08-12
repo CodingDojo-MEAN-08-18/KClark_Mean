91bcd0fadac4b9e5326a6afca223945fb3a3c654
Karen Clark <clarkkarenl@users.noreply.github.com>
2018-08-12 15:24:44 -0700

    Completed Deck of Cards assignment

--- deck_of_cards/deck_of_cards.js
+++ deck_of_cards/deck_of_cards.js
@@ -79,17 +79,93 @@ class Deck {
         }
         return deck;
     }
+
+    deal() {
+        return this.deck.pop();
+    }
+
+    cardCount() {
+        return this.deck.length;
+    }
 }
 
 // Testing Deck class!
-var deck1 = new Deck();
-console.log('New deck: ');
-console.log(deck1);
+// var deck1 = new Deck();
+// console.log('New deck: ');
+// console.log(deck1);
+// deck1.shuffle();
+// console.log('Shuffled deck: ');
+// console.log(deck1);
+// deck1.reset();
+// console.log('Reset deck: ');
+// console.log(deck1);
+// console.log('Deck card count: ');
+// console.log(deck1.cardCount());
+// console.log('Deal one: ');
+// console.log(deck1.deal());
+// console.log('Dealt one from deck: ');
+// console.log(deck1);
+// console.log('Deck card count: ');
+// console.log(deck1.cardCount());
+// console.log('Deal two: ');
+// console.log(deck1.deal());
+// console.log('Dealt another from deck: ');
+// console.log(deck1);
+// console.log('Deck card count: ');
+// console.log(deck1.cardCount());
 
-deck1.shuffle();
-console.log('Shuffled deck: ');
-console.log(deck1);
+// Part III
+// Now create a Player class. A Player should have the following functionality:
+// * The Player should have a name
+// * The Player should have a hand (an array of cards taken from a Deck)
+// * The Player should be able to take a Card (use the deck.deal method)
+// * The Player should be able to discard a Card
+class Player {
+    constructor(name) {
+        this._name = name;
+        this._hand = [];
+    }
 
-deck1.reset();
-console.log('Reset deck: ');
-console.log(deck1);
+    makeHand(deck, numCards) {
+        this.deck = deck;
+        this.numCards = numCards;
+
+        for (let count = 1; count < numCards + 1; count++ ) {
+            let newCard = deck.deal();
+            this._hand.push(`${newCard}`);
+        }
+    }
+
+    takeCard(deck) {
+        this.deck = deck;
+        let newCard = deck.deal();
+        this._hand.push(`${newCard}`);
+    }
+
+    discardCard() {
+        this._hand.pop();
+    }
+}
+
+// Testing Player
+// var player1 = new Player('Masako');
+// console.log(player1._name);
+// var deck1 = new Deck();
+// deck1.shuffle();
+// console.log(deck1);
+// console.log(deck1.cardCount());
+// player1.makeHand(deck1, 5);
+// console.log(player1._hand);
+// console.log(deck1);
+// console.log(deck1.cardCount());
+// player1.takeCard(deck1);
+// console.log(player1._hand);
+// console.log(deck1);
+// console.log(deck1.cardCount());
+// console.log("Orig hand: ");
+// console.log(player1._hand);
+// player1.discardCard();
+// console.log("New hand: ");
+// console.log(player1._hand);
+// console.log("Deck card count: ")
+// console.log(deck1.cardCount());
