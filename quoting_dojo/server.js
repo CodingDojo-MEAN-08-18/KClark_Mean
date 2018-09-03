0aad960c77fdb7a25a605917020367568d509ace
Karen Clark <clarkkarenl@users.noreply.github.com>
2018-09-03 14:55:18 -0700

    Vastly improved but no messages yet

--- quoting_dojo/server.js
+++ quoting_dojo/server.js
@@ -5,10 +5,10 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const flash = require('express-flash');
-app.use(bodyParser.urlencoded({ extended: true }));
+const path = require('path');
 app.use(flash());
 
-const path = require('path');
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, './static')));
 app.set('views', path.join(__dirname, './views'));
@@ -20,16 +20,13 @@ app.use(flash());
 
 // basic_mongoose DB set up
 mongoose.connect('mongodb://localhost/quotes');
-const QuotesSchema = new mongoose.Schema({
+const QuoteSchema = new mongoose.Schema({
     name: { type: String, required: true, minlength: 3 },
     quote: { type: String, required: true, min: 1, max: 255 }
 }, { timestamps: true });
-mongoose.model('Quotes', QuotesSchema);
-const Quote = mongoose.model('Quotes');
 
-// TODO: I am not happy making everything plural but it's
-// confusing having 'quote' and 'quotes'
-// Fix this once it gets working!
+mongoose.model('Quotes', QuoteSchema);
+const Quote = mongoose.model('Quotes');
 
 // default for Promises
 mongoose.Promise = global.Promise;
@@ -41,32 +38,27 @@ app.get('/', function(req, res) {
 
 // GET '/quotes' for the screen where all the quotes are rendered
 app.get('/quotes', function(req, res) {
-    Quote.find({}, function(err, quotes){
-        // if(err) {
-        //     let messages = [];
-        //     console.log("Something went wrong", err);
-        //     messages.push(err); 
-        //     res.render('index', { messages: messages });
-        // } else {
+    Quote
+    .find()
+    .sort({ createdAt : -1 })
+    .exec(function(err, quotes) {
+        if(err) {
+            console.log(err);
+        } else {
             res.render('quotes', { quotes: quotes });
-        // }
+        }
     });
 });
 
 // POST '/quotes' for the method of the form to make a new quote 
 app.post('/quotes', function(req, res) {
-    console.log("post data: ", req.body);
     const quote = new Quote({ name: req.body.name, quote: req.body.quote});
     quote.save(function(err){
         if(err) {
-            let messages = [];
-            console.log("Something went wrong", err);
-            messages.push(err); 
-            res.render('index', { messages: messages });
+            console.log(err);
         } else {
-            console.log("Added quotes");
-            res.render('quotes', { quotes: quotes });
-            // res.render('quotes');
+            console.log("Added quote");
+            res.redirect('/quotes');
         }
     });
 });
