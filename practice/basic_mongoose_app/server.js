ca9a9a6041b552af2405bce8b28fc7a0121d33d3
Karen Clark <clarkkarenl@users.noreply.github.com>
2018-09-02 17:02:47 -0700

    Trying to get flash msgs working. Validation works.

--- practice/basic_mongoose_app/server.js
+++ practice/basic_mongoose_app/server.js
@@ -1,22 +1,31 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const flash = require('express-flash');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(flash());
 
-var path = require('path');
+const path = require('path');
+
 app.use(express.static(path.join(__dirname, './static')));
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
+app.use(cookieParser('secret'));
+app.use(session({cookie: { maxAge: 60000 }, secret: 'secret'}));
+app.use(flash());
+
 // basic_mongoose DB set up
 mongoose.connect('mongodb://localhost/basic_mongoose');
-var UserSchema = new mongoose.Schema({
-    name: String,
-    age: Number
+const UserSchema = new mongoose.Schema({
+    name: { type: String, required: true, minlength: 3 },
+    age: { type: Number, required: true, min: 1, max: 150 }
 })
 mongoose.model('User', UserSchema);
-var User = mongoose.model('User');
+const User = mongoose.model('User');
 
 // default for Promises
 mongoose.Promise = global.Promise;
@@ -26,27 +35,36 @@ mongoose.Promise = global.Promise;
 app.get('/', function(req, res) {
     User.find({}, function(err, users){
         if(err) {
-            console.log("Unable to find documents in Users");
+            console.log("Something went wrong", err);
+            for(const key in err.errors) {
+                req.flash('errs', err.errors[key].message);
+            }
+            res.redirect('index');
         } else {
             res.render('index', { users: users });
         }
-    })
-})
+    });
+});
+
 // Add User Request 
 app.post('/users', function(req, res) {
     console.log("POST DATA", req.body);
-    // This is where we would add the user from req.body to the database.
-    var user = new User({ name: req.body.name, age: req.body.age});
+    const user = new User({ name: req.body.name, age: req.body.age});
     user.save(function(err){
         if(err) {
-            console.log("something went wrong");
+            console.log("Something went wrong", err);
+            for(const key in err.errors) {
+                req.flash('errs', err.errors[key].message);
+            }
+            res.redirect('/');
         } else {
             console.log("Successfully added user!");
             res.redirect('/');
         }
-    })
-})
+    });
+});
+
 // Setting our Server to Listen on Port: 8000
 app.listen(8000, function() {
     console.log("listening on port 8000");
-})
+});
