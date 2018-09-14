ef4d190132a94cc5b1abee57c40a9ac2aed08848
Karen Clark <clarkkarenl@users.noreply.github.com>
2018-09-13 22:45:36 -0700

    Moved TaskService to root component.

--- restful_tasks_contd/public/src/app/app.component.ts
+++ restful_tasks_contd/public/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Task } from './task';
+import { TaskService } from './task.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Restful Tasks Continued';
+
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit() {
+    this.getTasks();
+  }
+
+  getTasks(): void {
+    this.taskService.getTasks()
+      .subscribe(tasks => this.tasks = tasks);
+  }
 }
